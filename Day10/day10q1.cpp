fcad7dd1730f2c9c1bf69d0fd05f6046a625b684
neha <25110cn109@glbitm.ac.in>
2026-06-10 21:15:47 +0530

    Add files via upload

---
+++ Day10/day10q1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<n;j++) cout<<" ";
+        for(int j=1;j<=2*i-1;j++) cout<<"*";
+        cout<<"\n";
+    }
+}
