1a17c20366c025ad142bb919dcd6dd82d1ce52fd
neha <25110cn109@glbitm.ac.in>
2026-06-17 21:48:00 +0530

    Add files via upload

---
+++ day15/day15q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=5;
+
+    int temp=arr[0];
+
+    for(int i=0;i<n-1;i++)
+        arr[i]=arr[i+1];
+
+    arr[n-1]=temp;
+
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+}
