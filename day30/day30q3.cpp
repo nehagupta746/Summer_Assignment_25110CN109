2bb169fdb4025f1fcc4a845d099dd6562c02a61d
neha <25110cn109@glbitm.ac.in>
2026-06-30 18:32:00 +0530

    Add files via upload

---
+++ day30/day30q3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int count=0;
+
+    while(n>0){
+
+        count++;
+        n/=10;
+    }
+
+    cout<<count;
+
+    return 0;
+}
