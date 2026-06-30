036b395cd5350a8b6e5f5b8a770d7c740e5edfd6
neha <25110cn109@glbitm.ac.in>
2026-06-30 18:27:05 +0530

    Add files via upload

---
+++ day27/day27q2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    char op;
+
+    cin>>a>>op>>b;
+
+    switch(op){
+
+        case '+':
+            cout<<a+b;
+            break;
+
+        case '-':
+            cout<<a-b;
+            break;
+
+        case '*':
+            cout<<a*b;
+            break;
+
+        case '/':
+            cout<<a/b;
+            break;
+
+        default:
+            cout<<"Invalid Operator";
+    }
+
+    return 0;
+}
